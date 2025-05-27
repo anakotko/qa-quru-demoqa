d617279a40790f3ff354e160211f62390ef2bb4c
Anastasiia Kotko <mac@MacBook-Pro.local>
2025-05-27 20:36:34 +0700

    added hw11

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 import pages.components.CalendarComponent;
 import pages.components.CheckResultComponent;
 
@@ -29,6 +30,7 @@ public class RegistrationPage {
     CalendarComponent calendarComponent = new CalendarComponent();
     CheckResultComponent checkResultComponent = new CheckResultComponent();
 
+    @Step("Открываем главную страницу")
     public RegistrationPage openPage(){
         open("/automation-practice-form");
         executeJavaScript("$('#fixedban').remove()");
@@ -37,36 +39,42 @@ public class RegistrationPage {
         return this;
     }
 
+    @Step("Устанавливаем значение в поле First Name")
     public RegistrationPage setFirstName(String value){
         firstNameInput.setValue(value);
 
         return this;
     }
 
+    @Step("Устанавливаем значение в поле Last Name")
     public RegistrationPage setLastName(String value){
         lastNameInput.setValue(value);
 
         return this;
     }
 
+    @Step("Устанавливаем значение в поле Email")
     public RegistrationPage setEmail(String value){
         userEmailInput.setValue(value);
 
         return this;
     }
 
+    @Step("Устанавливаем значение в поле Gender")
     public RegistrationPage setGender(String value){
         genderWrapper.$(byText(value)).click();
 
         return this;
     }
 
+    @Step("Устанавливаем значение в поле Mobile")
     public RegistrationPage setUserNumber(String value){
         userNumberInput.setValue(value);
 
         return this;
     }
 
+    @Step("Устанавливаем дату в поле Date of Birth")
     public RegistrationPage setDateOfBirth(String day, String month, String year){
         calendarInput.click();
         calendarComponent.setDate(day, month, year);
@@ -74,30 +82,36 @@ public class RegistrationPage {
         return this;
     }
 
+    @Step("Выбираем предмет")
     public RegistrationPage setSubjects(String value){
         subjectsInput.setValue(value).pressEnter();
 
         return this;
     }
 
+    @Step("Выбираем хобби")
     public RegistrationPage setHobbies(String value){
         hobbiesCheckBox.setValue(value).click();
 
         return this;
 
     }
+
+    @Step("Загружаем картинку")
     public RegistrationPage setPicture(){
         pictureButton.uploadFromClasspath("img.png");
 
         return this;
     }
 
+    @Step("Устанавливаем значение в поле Address")
     public RegistrationPage setAddress(String value){
         addressInput.setValue(value);
 
         return this;
     }
 
+    @Step("Выбираем штат")
     public RegistrationPage setState(String value){
         selectState.click();
         stateInput.setValue(value).pressEnter();
@@ -105,6 +119,7 @@ public class RegistrationPage {
         return this;
     }
 
+    @Step("Выбираем город")
     public RegistrationPage setCity(String value){
         selectCity.click();
         cityInput.setValue(value).pressEnter();
@@ -112,10 +127,12 @@ public class RegistrationPage {
         return this;
     }
 
+    @Step("Отправляем форму")
     public void clickSubmitBtn() {
         submitBtn.click();
     }
 
+    @Step("Проверка таблицы")
     public RegistrationPage checkResult(String key, String value){
 
         checkResultComponent.checkResultFullForm(key, value);
